1f5f8edea61cdb34fb8b18ff73f465e4912217d7
Julián Melero Hidalgo <hola@julianmelero.com>
2022-08-05 17:39:54 +0200

    Mock

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -7,12 +7,18 @@ const fakeBooks = [
   },
 ];
 
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+const mockGetAll = jest.fn();
+
+/* const MongoLibStub = {
+  getAll: mockGetAll,
   create: () => {},
 };
+*/
 
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
 describe('Test for BooksService', () => {
   let service;
@@ -22,9 +28,12 @@ describe('Test for BooksService', () => {
 
   describe('Test for GetBooks', () => {
     test('should return a list of books', async () => {
+      mockGetAll.mockResolvedValue(fakeBooks);
       const books = await service.getBooks({});
       console.log(books);
       expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
   });
 });
